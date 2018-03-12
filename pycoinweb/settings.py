4b7e626c8904fcaad4fc9974dc7ee8160edbecf0
liushuang <liushuangqwe1117@163.com>
2018-03-12 22:31:55 +0800

    时区设置

--- pycoinweb/settings.py
+++ pycoinweb/settings.py
@@ -135,7 +135,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+# 设置为True表示使用系统的时区
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
